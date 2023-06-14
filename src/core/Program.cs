d02d8c01589d223fd83992daf629b5aab658a220
Tim Hanewich <tahanewich@live.com>
2023-06-14 08:53:52 -0400

    Added core console app

---
+++ src/core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartHomeCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hi!");
+        }
+    }
+}
