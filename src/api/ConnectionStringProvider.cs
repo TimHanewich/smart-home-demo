1ff120c6b1412ab82ab561cad1049e95a70390b8
Tim Hanewich <tahanewich@live.com>
2023-06-14 08:48:02 -0400

    Added ConnectionStringProvider

---
+++ src/api/ConnectionStringProvider.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SmartHomeApi
+{
+    public class ConnectionStringProvider
+    {
+        public string SqlConnectionString
+        {
+            get
+            {
+                return "(paste your SQL connection string here)";
+            }
+        }
+    }
+}
